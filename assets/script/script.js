97602d68e7d85e129703c6fe93e0b2b678c9d581
victorhenrie <victorhrdsilva@gmail.com>
2022-04-17 19:27:09 -0300

    solves bug

--- assets/script/script.js
+++ assets/script/script.js
@@ -30,7 +30,7 @@ function crypto(text) {
 }
 
 function decryption(text) {
-    var alphabet = [' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var newText = "";
     for (var i = 0; i < text.length; i++) {
         for (var f = 0; f < alphabet.length; f++) {
